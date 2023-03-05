4145ea2145cd4b2b0f986d37198b52e9b128eae7
QaboneLama <thomas.schwabauer@gmx.net>
2023-03-05 20:13:10 +0100

    added exchange deposit event

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -1,12 +1,36 @@
 //SPDX-License-Identifier: UNLICENSE
 pragma solidity ^0.8.0; 
 
+import "./Token.sol";
+
 contract Exchange {
     address public feeAccount;
     uint8 public feePercent;
 
+    mapping(address => mapping(address => uint)) public tokens;
+
+    event Deposit(
+        address _token,
+        address _user,
+        uint _amount,
+        uint _balance
+    );
+
     constructor(address _feeAccount, uint8 _feePercent) {
         feeAccount = _feeAccount;
         feePercent = _feePercent;
+    }
+
+
+    function depositToken(address _tokenAddress, uint _amount) public {
+        Token(_tokenAddress).transferFrom(msg.sender, address(this), _amount);
+        tokens[_tokenAddress][msg.sender] += _amount;
+
+        emit Deposit(_tokenAddress, msg.sender, _amount, balanceOf(_tokenAddress, msg.sender));
+    }
+
+
+    function balanceOf(address _token, address _user) public view returns (uint) {
+        return tokens[_token][_user];
     }
 }
